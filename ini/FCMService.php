75894d060fafe49d8dabe8ac40c106d57e499396
Chigozie peter <ujahchigozie@gmail.com>
2018-12-11 21:01:57 +0800

    Pushing message to firebase server

---
+++ ini/FCMService.php
@@ -0,0 +1,44 @@
+<?php 
+/**
+ * Created by SHELL on 11/12/2018.
+ * Developer name ujah chigozie 
+ */
+ namespace UjahPeter{
+	 class Firebase {
+		 
+		 private $FirebaseApikey;
+		 private $FirebaseURL =  'https://fcm.googleapis.com/fcm/send';
+		 
+		 public function __construct($FirebaseApikey) {
+			$this->FirebaseApikey = $FirebaseApikey;
+		}
+
+		 public function send($sendPushArray, $type = "single") {
+			$sendPushArray =  (($type == "multiple") ?  (array) $sendPushArray : $sendPushArray);
+			return $this->sendPushNotification($sendPushArray);
+		}
+		 
+		private function sendPushNotification($sendFields) {
+			$headers = array(
+				'Authorization: key=' . $this->FirebaseApikey,
+				'Content-Type: application/json'
+			); 
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $this->FirebaseURL);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);   
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_POSTFIELDS,  json_encode($sendFields));
+			$result = curl_exec($ch);               
+			if ($result === false) {
+				return 'Curl failed: ' . curl_error($ch);
+			}
+			curl_close($ch);
+			return $result;
+		}
+	 
+	 }
+ }
